882b954520b8d1dd678599c5c3c73b6721a8858f
kym <kym@kym-laptop.(none)>
2012-11-17 19:32:38 +1100

    iniitalise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equitably::Application.config.secret_token = '49a13feb8b668d981e9a84cab5b8e313ad61ae6a9fde35e9888090ac68372ac252cf5d2618d66da217d8c354d581bc0299a6bf263b718560c41cc46b8091cf02'
